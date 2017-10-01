01b5d0d4b5c10bed9cc5b6525af2c7393f139b82
endor-force <1937941+endor-force@users.noreply.github.com>
2017-10-01 14:58:36 +0200

    Experimental script
    Use on debug ssh on host to install tellstick files and autostart telldusd on homeassistant container.

---
+++ telldus-core-2.1.2_bin_armhf_resinOS/install_telldus-core.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Installing dependencies..."
+docker exec homeassistant apk add --no-cache confuse libftdi1 libstdc++
+
+echo "Copying telldus binaries, libraries and config files..."
+docker exec homeassistant cp -R /share/telldus-core/etc /etc
+docker exec homeassistant cp -R /share/telldus-core/usr /usr
+docker exec homeassistant cp -R /share/telldus-core/var /var
+
+echo "Adding telldusd service to start up on container start..."
+docker exec homeassistant awk '/udevadm trigger/ { print; print "telldusd &"; next}1' /usr/bin/entry.sh > /resin-data/share/tempentry.sh
+docker exec homeassistant cp /share/tempentry.sh /usr/bin/entry.sh
+docker exec homeassistant chmod +x /usr/bin/entry.sh
+docker exec homeassistant rm /share/tempentry.sh
+
+
+echo "Restarting container homeassistant.."
+docker restart homeassistant
+
+echo "Done!"
+echo "Please wait a few minutes and then reload your hass.io with your browser!"
